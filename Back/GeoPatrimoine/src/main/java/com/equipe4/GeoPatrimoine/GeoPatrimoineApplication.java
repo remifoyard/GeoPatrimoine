eae7cf176ca326ca14e4916d91232565ebf370fa
remifoyard <rfpro8@gmail.com>
2018-04-23 14:33:32 +0200

    Base Project Maven

---
+++ Back/GeoPatrimoine/src/main/java/com/equipe4/GeoPatrimoine/GeoPatrimoineApplication.java
@@ -0,0 +1,12 @@
+package com.equipe4.GeoPatrimoine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoPatrimoineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoPatrimoineApplication.class, args);
+	}
+}
