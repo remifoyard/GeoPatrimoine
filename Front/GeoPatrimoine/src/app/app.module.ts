0d426808f08c18d8b881515507707fe800ddc0c3
lorenzo.llopis <lorenzo.llopis@cgi.com>
2018-06-25 14:27:22 +0200

    Creation Map Component + Leaflet Npm

--- Front/GeoPatrimoine/src/app/app.module.ts
+++ Front/GeoPatrimoine/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
--- Front/GeoPatrimoine/src/app/material/material.module.ts
+++ Front/GeoPatrimoine/src/app/material/material.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @NgModule({
   imports: [
     CommonModule
