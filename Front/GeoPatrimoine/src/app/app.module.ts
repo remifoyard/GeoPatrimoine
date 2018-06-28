538198c14fa574e23912ef43f284ba68b4f7226e
lorenzo.llopis <lorenzo.llopis@cgi.com>
2018-06-28 12:00:51 +0200

    [Feat][Front/ListComponent]Création du composant

--- Front/GeoPatrimoine/src/app/app.module.ts
+++ Front/GeoPatrimoine/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import {MaterialModule} from './material/material.module';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,LeafletModule.forRoot(),MaterialModule
