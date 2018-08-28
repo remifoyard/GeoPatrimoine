3e7aae3fb807e20694acadd4535f6903235d9827
lorenzo.llopis <lorenzo.llopis@cgi.com>
2018-08-28 10:23:08 +0200

    feat[Theming]css vers sass /ajout couleurs charte graphique

--- Front/GeoPatrimoine/src/app/app.component.ts
+++ Front/GeoPatrimoine/src/app/app.component.ts
@@ -9,14 +9,25 @@ import {HttpClient} from '@angular/common/http';
 export class AppComponent {
   title = 'app';
   arrData: string [];
-
+url:string;
   constructor(private http: HttpClient){
 
   }
+  
 
   ngOnInit(){
     this.http.get('http://localhost:8080/api/patrimoineHistorique').subscribe(response=>{
       this.arrData = response as string [];	   
     console.log(response[0].nom)});
   }
+
+
+  
+  search(searchTag){
+  this.url='http://localhost:8080/api/patrimoineHistoriqueTag/'+searchTag
+this.http.get(this.url).subscribe(response=>{
+  this.arrData = response as string [];	   
+  });}
+
+
 }
